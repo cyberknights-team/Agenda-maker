ef4306d39df0a31579cd635f5fd6ae7de8b9a359
logaprakash <logaprakash98@gmail.com>
2016-10-02 15:49:57 +0530

    all

---
+++ agenda_maker_windows_v01/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace agenda_maker_windows_v01
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                StorageFile file = await ApplicationData.Current.LocalFolder.GetFileAsync("keys.ini");
+                List<storage> items = new List<storage>();
+                string temp = await Windows.Storage.FileIO.ReadTextAsync(file);
+                string[] arr = temp.Split('\n');
+
+                foreach (string s in arr)
+                {
+                    string[] substring = s.Split('\n');
+                    
+                   
+                    storage item = new storage(substring[0],substring[1]);
+                    items.Add(item);
+                }
+                list.ItemsSource = items;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void add_agenda_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(add_agenda));
+        }
+    }
+}
---
+++ agenda_maker_windows_v01/storage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agenda_maker_windows_v01
+{
+    public class storage
+    {
+        public string title { get; set; }
+        public string description { get; set; }
+
+        public storage() { }
+        public storage(string Title,string Description)
+        {
+            this.title = Title;
+            this.description = Description;
+        }
+    }
+}
